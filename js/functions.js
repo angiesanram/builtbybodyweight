7770c167cafd09017a72bbe342ac1d24665b3fb6
angiesanram <mpedwards.me@gmail.com>
2017-07-06 22:45:04 +1000

    major update 1

--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,18 @@
 // Menu
 
 $(document).ready(function() {
-	$('.mobile-nav-toggle').click(function() {
-	$(this).toggleClass('is-open');
-	$('.mobile-nav').toggleClass('is-open');
+		$('.mobile-nav-toggle').click(function() {
+			$(this).toggleClass('is-open');
+			$('.mobile-nav').toggleClass('is-open');
+	});
 });
+
+
+
+$(document).ready(function() {
+		$('.site-nav a').click(function() {
+			$(this).toggleClass('active');
+			$('.site-nav a').toggleClass('active');
+	});
+
 });
