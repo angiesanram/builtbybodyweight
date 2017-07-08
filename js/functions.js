b6393ac779291e54f8792d6ecf3339f2d760bcd7
angiesanram <mpedwards.me@gmail.com>
2017-07-08 17:08:56 +1000

    c

--- js/functions.js
+++ js/functions.js
@@ -6,13 +6,3 @@ $(document).ready(function() {
 			$('.mobile-nav').toggleClass('is-open');
 	});
 });
-
-
-
-$(document).ready(function() {
-		$('.site-nav a').click(function() {
-			$(this).toggleClass('active');
-			$('.site-nav a').toggleClass('active');
-	});
-
-});
