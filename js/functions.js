19d6fdc95e672fb5c948c6bc69cfc60092357e82
angiesanram <mpedwards.me@gmail.com>
2017-07-09 22:25:36 +1000

    c

--- js/functions.js
+++ js/functions.js
@@ -6,3 +6,18 @@ $(document).ready(function() {
 			$('.mobile-nav').toggleClass('is-open');
 	});
 });
+
+//clickable coaching
+$(document).ready(function() {
+	$('.12-week-transformation').click(function(){
+	  alert('12 week Clicked !!');
+	});
+
+	$('.skills').click(function(){
+	  alert('skills Clicked !!');
+	});
+
+	$('.personal-coaching').click(function(){
+	  alert('personal Clicked !!');
+	});
+});
