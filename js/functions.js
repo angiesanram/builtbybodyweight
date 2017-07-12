0b78866862529da051a721d426d2d804aad96272
angiesanram <mpedwards.me@gmail.com>
2017-07-12 18:47:55 +1000

    home-why added

--- js/functions.js
+++ js/functions.js
@@ -21,3 +21,40 @@ $(document).ready(function() {
 	  alert('personal Clicked !!');
 	});
 });
+
+$(document).ready(function() {
+
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+
+        /* Check the location of each desired element */
+        $('.flex-container').each( function(i){
+
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+
+							$(".list li").each(function(i) {
+								/*$(this).animate({'opacity':'1'},500);*/
+								$(this).delay(500 * i).animate({'opacity':'0.7'},500)
+								$(this).off('scroll');
+								$(this).css('pointer-events', 'auto');
+								/*$(this).delay(800 * i).fadeIn(800);*/
+							});
+
+            }
+
+        });
+
+    });
+
+});
+
+/*if ($(window).scrollTop() >= triggerHeight) {
+	$(".list li").each(function(i) {
+		$(this).delay(300 * i).fadeIn(500);
+	});
+	$(this).off('scroll');
+}*/
